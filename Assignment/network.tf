e0689d894c385623193f5c753b26f5a19c8f4f35
snehal.palve <snehal.palve@xoriant.com>
2024-03-03 01:09:10 +0530

    Adding count object for db connection for each VM

--- Assignment/network.tf
+++ Assignment/network.tf
@@ -1,15 +1,8 @@
 # Create Azure Virtual Network Rule to allow access from app tier
-resource "azurerm__virtual_network_rule" "app_network_rule" {
-  name                = "app-network-rule"
-  server_name         = azurerm_mssql_server.database_server.name
-  resource_group_name = var.azurerm_resource_group
-  subnet_id           = azurerm_subnet.app_subnet.id
-}
-
-resource "azurerm_lb" "web_lb" {
-  name                = "webloadbalancer"
-  location            = var.location
-  resource_group_name = var.azurerm_resource_group
+resource "azurerm_mssql_virtual_network_rule" "app_network_rule" {
+  name      = "sql-vnet-rule"
+  server_id = azurerm_mssql_server.database_server.id
+  subnet_id = azurerm_subnet.app_subnet.id
 }
 
 # Connect the web tier to the application tier
@@ -18,7 +11,7 @@ resource "azurerm_lb_backend_address_pool" "web_backend_pool" {
   loadbalancer_id     = azurerm_lb.web_lb.id
 
   dynamic "backend_addresses" {
-    for_each = azurerm_virtual_machine.VM-DEV-WEB[0].id
+    for_each = azurerm_windows_virtual_machine.VM-DEV-WEB[count.index]
     content {
       ip_address = backend_addresses.value.private_ip_address
     }
@@ -28,7 +21,7 @@ resource "azurerm_lb_backend_address_pool" "web_backend_pool" {
 # Connect the application tier to the database tier
 resource "azurerm_virtual_machine_extension" "app_db_extension" {
   name                 = "app-db-extension"
-  virtual_machine_id   = azurerm_virtual_machine.VM-DEV-APP[0].id
+  virtual_machine_id   = azurerm_windows_virtual_machine.VM-DEV-APP[count.index].id
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.0"
@@ -43,7 +36,7 @@ resource "azurerm_virtual_machine_extension" "app_db_extension" {
 # Configure database connection string in the application tier
 resource "azurerm_virtual_machine_extension" "app_db_connection" {
   name                 = "app-db-connection"
-  virtual_machine_id   = azurerm_virtual_machine.VM-DEV-APP[0].id
+  virtual_machine_id   = azurerm_windows_virtual_machine.VM-DEV-APP[count.index].id
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.0"
