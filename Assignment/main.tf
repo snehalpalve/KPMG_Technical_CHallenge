f9e2b6648c7894681a0aa9acb2ae66ba26a6103a
snehal.palve <snehal.palve@xoriant.com>
2024-03-02 06:39:30 +0530

    Adding vnet

--- Assignment/main.tf
+++ Assignment/main.tf
@@ -15,3 +15,11 @@ resource "azurerm_resource_group" "KPMG" {
     name = "KPMG"
     location= "West US"
 }
+
+# Create Azure Virtual Network
+resource "azurerm_virtual_network" "VNET-KPMG" {
+  name                = "VNET-KPMG"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus" 
+  resource_group_name = KPMG
+}
